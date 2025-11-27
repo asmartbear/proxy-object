17e71c3d066a1fbb5591a9fc070208ebfc78a841
Jason Cohen <github@asmartbear.com>
2025-11-27 17:22:48 -0600

    Started general array, with unshift()

--- src/array.ts
+++ src/array.ts
@@ -1,60 +1,84 @@
 import * as D from '@asmartbear/dyn'
 
-/** Implementation of an Array proxy */
-export interface IArrayImplementation<T> {
+/** Implementation of an Array type where "append" is the only allowed operation. */
+export interface IArrayAppendOnlyImplementation<T> {
     /** Returns any iterable over all elements in the array. */
     elements(): Iterable<T>
-    /** Sets the element at a specific index, which is within the array bounds. */
-    set(idx: number, x: T): void
-    /** Inserts an element at the given location, which will be in `[0,length]` inclusive; `length` if appending. */
+    /** Inserts an element at the end of the array. */
+    append(x: T): void
+}
+
+/** Implementation of an Array type. */
+export interface IArrayImplementation<T> extends IArrayAppendOnlyImplementation<T> {
+    /** Inserts an element at the specified index, shifting out elements currently at or later. */
     insert(idx: number, x: T): void
 }
 
-/** Proxy object that acts like a native generic object */
-export class ProxyArray<T> {
+/** Proxy object that acts like a native generic object, except read-only except for push(). */
+export class ProxyArrayAppendOnly<T, IMPL extends IArrayAppendOnlyImplementation<T>> extends Array<T> {
 
-    private readonly impl: IArrayImplementation<T>
-    private proxyActive: boolean
+    protected readonly impl: IMPL
+    protected state: { proxyActive: boolean }
 
-    private constructor(impl: IArrayImplementation<T>) {
-        this.impl = impl
-        this.proxyActive = true
-    }
-
-    static from<T>(impl: IArrayImplementation<T>): T[] {
-        const obj = D.ARRAY(impl.elements())
-        return new Proxy<T[]>(obj, new ProxyArray(impl))
-    }
-
-    get(target: T[], p: string | symbol, receiver: any): any {
-        switch (p) {
-            // case 'unshift':
-            //     return this.unshift.bind(receiver)
-            default:
-                return Reflect.get(target, p, receiver)
+    protected constructor(impl: IMPL) {
+        const arr = D.ARRAY(impl.elements())
+        super(arr.length)
+        for (const i in arr) {
+            this[i] = arr[i]
         }
+        this.impl = impl
+        this.state = { proxyActive: true }
+    }
+
+    static createAppendOnly<T, IMPL extends IArrayAppendOnlyImplementation<T>>(impl: IMPL): T[] {
+        const obj = new ProxyArrayAppendOnly<T, IMPL>(impl)
+        return new Proxy<T[]>(obj, obj)
     }
 
     set(target: T[], p: any, x: any, receiver: any): boolean {
-        if (p !== 'length') {
-            const idx = Number.parseInt(p)
-            if (idx >= target.length) {          // appending means "setting" an element at the end of the array
-                this.impl.insert(target.length, x)
-            } else {
-                this.impl.set(idx, x)
-            }
+        if (this.state.proxyActive) {
+            throw new Error("Cannot set elements in this type of array: " + this.constructor.name)
         }
         return Reflect.set(target, p, x, receiver)
     }
 
-    unshift(target: T[], ...elements: T[]): number {
-        // With insertions
-        for (let i = elements.length; --i >= 0;) {
-            this.impl.insert(0, elements[i])
+    push(...items: T[]): number {
+        if (items.length <= 0) return 0       // trivial case
+        for (const x of items) {
+            this.impl.append(x)
         }
+        this.state.proxyActive = false
+        const result = super.push(...items)
+        this.state.proxyActive = true
+        return result
+    }
 
-        // Do the default for the actual array
-        return target.unshift(...elements)
+}
+
+/** Proxy object that acts like a native generic object */
+export class ProxyArray<T> extends ProxyArrayAppendOnly<T, IArrayImplementation<T>> {
+
+    static create<T, IMPL extends IArrayImplementation<T>>(impl: IMPL): T[] {
+        const obj = new ProxyArray<T>(impl)
+        return new Proxy<T[]>(obj, obj)
+    }
+
+    set(target: T[], p: any, x: any, receiver: any): boolean {
+        if (this.state.proxyActive) {
+            throw new Error("Cannot set elements in this type of array: " + this.constructor.name)
+        }
+        return Reflect.set(target, p, x, receiver)
+    }
+
+    unshift(...items: T[]): number {
+        if (items.length <= 0) return 0       // trivial case
+        for (let i = items.length; --i >= 0;) {
+            this.impl.insert(0, items[i])
+        }
+        this.state.proxyActive = false
+        const result = super.unshift(...items)
+        this.state.proxyActive = true
+        return result
     }
 
 }
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,61 +1,117 @@
 import * as T from '@asmartbear/testutil'
-import { IArrayImplementation, ProxyArray } from "../src/array"
+import { IArrayAppendOnlyImplementation, ProxyArrayAppendOnly, IArrayImplementation, ProxyArray } from "../src/array"
 
-class UnitArrayImpl implements IArrayImplementation<string> {
-    public readonly list: string[]
+class UnitArrayAppendOnlyImpl<T> implements IArrayAppendOnlyImplementation<T> {
+    public readonly list: T[]
     public counters: {
-        sets: number,
-        inserts: number,
-        deletes: number,
+        appends: number,
     }
 
-    constructor(init: string[]) {
+    constructor(init: T[]) {
         this.list = init.slice()
-        this.counters = { sets: 0, inserts: 0, deletes: 0 }
+        this.counters = { appends: 0 }
     }
-    elements(): Iterable<string> {
+    elements(): Iterable<T> {
         return this.list
     }
-    set(idx: number, value: string): void {
-        T.is(idx >= 0)
-        T.is(idx < this.list.length)
-        this.list[idx] = value
-        ++this.counters.sets
+    append(value: T): void {
+        this.list.push(value)
+        ++this.counters.appends
     }
-    insert(idx: number, value: string): void {
-        T.is(idx >= 0)
-        T.is(idx <= this.list.length)
+}
+
+class UnitArrayImpl<T> extends UnitArrayAppendOnlyImpl<T> implements IArrayImplementation<T> {
+    public counters: {
+        appends: number,
+        inserts: number,
+    }
+
+    constructor(init: T[]) {
+        super(init)
+        this.counters = { appends: 0, inserts: 0 }
+    }
+    insert(idx: number, value: T): void {
         this.list.splice(idx, 0, value)
         ++this.counters.inserts
     }
 }
 
-test('basic array operations', () => {
-    const impl = new UnitArrayImpl(["a", "b", "c"])
-    const s = ProxyArray.from(impl)
+function doAppendTests(s: string[], impl: UnitArrayAppendOnlyImpl<string>) {
     T.len(s, 3)
     T.eq(s.toString(), "a,b,c")
-    T.eq(impl.counters, { sets: 0, inserts: 0, deletes: 0 })
+    T.includes(impl.counters, { appends: 0 })
 
-    // Set a value
-    s[1] = "bb"
+    // Trivial append
+    s.push()
     T.len(s, 3)
-    T.eq(s.toString(), "a,bb,c")
-    T.eq(impl.counters, { sets: 1, inserts: 0, deletes: 0 })
-    T.eq(impl.list.toString(), "a,bb,c")
+    T.eq(s.toString(), "a,b,c")
+    T.includes(impl.counters, { appends: 0 })
+    T.eq(impl.list.toString(), "a,b,c")
 
     // Append a value
     s.push("d")
     T.len(s, 4)
-    T.eq(s.toString(), "a,bb,c,d")
-    T.eq(impl.counters, { sets: 1, inserts: 1, deletes: 0 })
-    T.eq(impl.list.toString(), "a,bb,c,d")
+    T.eq(s.toString(), "a,b,c,d")
+    T.includes(impl.counters, { appends: 1 })
+    T.eq(impl.list.toString(), "a,b,c,d")
 
-    // Prepend a value
-    // s.unshift("e")
-    // T.len(s, 5)
-    // T.eq(s.toString(), "e,a,bb,c,d")
-    // T.eq(impl.counters, { sets: 1, inserts: 2, deletes: 0 })
-    // T.eq(impl.list.toString(), "e,a,bb,c,d")
+    // Append multiple values
+    s.push("e", "f")
+    T.len(s, 6)
+    T.eq(s.toString(), "a,b,c,d,e,f")
+    T.includes(impl.counters, { appends: 3 })
+    T.eq(impl.list.toString(), "a,b,c,d,e,f")
+
+    // Illegal for all arrays
+    T.throws(() => { s.length = 10 })
+}
+
+function doPrependTests(s: string[], impl: UnitArrayImpl<string>) {
+    T.len(s, 3)
+    T.eq(s.toString(), "a,b,c")
+    T.includes(impl.counters, { appends: 0, inserts: 0 })
+
+    // Trivial append
+    s.unshift()
+    T.len(s, 3)
+    T.eq(s.toString(), "a,b,c")
+    T.includes(impl.counters, { appends: 0, inserts: 0 })
+    T.eq(impl.list.toString(), "a,b,c")
+
+    // Append a value
+    s.unshift("d")
+    T.len(s, 4)
+    T.eq(s.toString(), "d,a,b,c")
+    T.includes(impl.counters, { appends: 0, inserts: 1 })
+    T.eq(impl.list.toString(), "d,a,b,c")
+
+    // Append multiple values
+    s.unshift("e", "f")
+    T.len(s, 6)
+    T.eq(s.toString(), "e,f,d,a,b,c")
+    T.includes(impl.counters, { appends: 0, inserts: 3 })
+    T.eq(impl.list.toString(), "e,f,d,a,b,c")
+}
+
+test('append-only array', () => {
+    const impl = new UnitArrayAppendOnlyImpl(["a", "b", "c"])
+    const s: string[] = ProxyArrayAppendOnly.createAppendOnly(impl)
+    doAppendTests(s, impl)
+
+    // Illegal for append-only
+    T.throws(() => { s[1] = "z" })
+})
+
+test('full array', () => {
+
+    let impl = new UnitArrayImpl(["a", "b", "c"])
+    let s: string[] = ProxyArray.create(impl)
+    doAppendTests(s, impl)
+
+    impl = new UnitArrayImpl(["a", "b", "c"])
+    s = ProxyArray.create(impl)
+    doPrependTests(s, impl)
+
+
 })
 
